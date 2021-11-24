c8559af40f5db4b6cc67e38b2320d1e22eae30dc
Jon <clap_that_ham@hotmail.com>
2021-11-24 20:28:30 +0000

    ticket UI

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -14,22 +14,27 @@ const start = async () => {
         throw new Error('NATS_CLUSTER_ID must be defined');
     }
 
-    await natsWrapper.connect(
-        process.env.NATS_CLUSTER_ID,
-        process.env.NATS_CLIENT_ID,
-        process.env.NATS_URL
-    );
+    try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
 
-    natsWrapper.client.on('close', () => {
-        console.log('NATS connection closed');
-        process.exit();
-    });
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed');
+            process.exit();
+        });
 
-    process.on('SIGINT', () => natsWrapper.client.close());
-    process.on('SIGTERM', () => natsWrapper.client.close());
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
 
-    new OrderCreatedListener(natsWrapper.client).listen();
+        new OrderCreatedListener(natsWrapper.client).listen();
+    } catch (err) {
+        console.error(err);
+  }
 };
+
 
 start();
 
